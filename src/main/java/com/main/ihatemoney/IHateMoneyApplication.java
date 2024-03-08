a59d6fd8e9039347e21367bc8908a16615474c76
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-03-08 09:43:02 +0700

    initialize project

---
+++ src/main/java/com/main/ihatemoney/IHateMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.main.ihatemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHateMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHateMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/ihatemoney/IHateMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.ihatemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHateMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

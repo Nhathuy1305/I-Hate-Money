1d07d6d148d4ee593f1274edaf2322654d1c2f77
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-04-27 15:25:31 +0700

    Config Data security, add data service

---
+++ src/main/java/com/main/ihatemoney/security/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.main.ihatemoney.security;
+
+import com.main.ihatemoney.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class SecurityConfig extends VaadinWebSecurity {
+
+    private static final String LOGIN_PROCESSING_URL = "/login";
+    private static final String LOGIN_FAILURE_URL = "/login?error";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        setLoginView(http, LoginView.class);    // Set the custom login view for the application
+        http.csrf().disable();
+        http
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl(LOGIN_PROCESSING_URL)
+                .failureUrl(LOGIN_FAILURE_URL)
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+
+        http.headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(
+                // Client-side JS
+                "/VAADIN/**",
+
+                // the standard favicon URI
+                "/favicon.ico",
+
+                // the robots exclusion standard
+                "/robots.txt",
+
+                // web application manifest
+                "/manifest.webmanifest",
+                "/sw.js",
+                "/offline.html",
+
+                // icons and images
+                "/icons/**",
+                "/images/**",
+                "/styles/**",
+
+                // (development mode) H2 debugging console
+                "/h2-console/**"
+        ); // ignore these requests
+        super.configure(web);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource) {
+            @Override
+            protected List<GrantedAuthority> loadUserAuthorities(String username) {
+                return AuthorityUtils.createAuthorityList("ROLE_USER");
+            }
+        };
+    }
+}

460e0ce44d2939674c36b0a3d95a5699cd2c3a14
Nhathuy1305 <ITITIU20043@student.hcmiu.edu.vn>
2024-04-27 16:34:05 +0700

    Build Template and Vite Config

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -1,6 +1,8 @@
-import './vaadin-featureflags';
+import './vaadin-featureflags.ts';
 
 import './index';
 
-import { applyTheme } from './theme';
+import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
+
+import { applyTheme } from './theme.js';
 applyTheme(document);
